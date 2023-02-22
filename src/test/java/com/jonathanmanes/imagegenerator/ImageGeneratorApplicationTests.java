7186e8ef1aa3e247a3ba45cafa677859614a06b1
Jonathan Manes <manesjona@gmail.com>
2023-02-22 13:45:08 +0100

    updating gititgnore and adding some files

---
+++ src/test/java/com/jonathanmanes/imagegenerator/ImageGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathanmanes.imagegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
