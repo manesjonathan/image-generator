2fdda8311ceb2ca72e7a857142c0a9f830063116
Jonathan Manes <manesjona@gmail.com>
2023-03-06 15:36:14 +0100

    Custom loading banner

--- src/main/java/com/jonathanmanes/imagegenerator/ImageGeneratorApplication.java
+++ src/main/java/com/jonathanmanes/imagegenerator/ImageGeneratorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ImageGeneratorApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ImageGeneratorApplication.class, args);
     }
 
